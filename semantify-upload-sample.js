895788a3ed3fe7711ee91416696975de9e03a9bb
Elias Kärle <elias@kaerle.com>
2017-06-14 11:25:18 +0200

    initial commit of sample upload file

---
+++ semantify-upload-sample.js
@@ -0,0 +1,50 @@
+var request = require("request");
+
+start();
+
+function start() {
+
+    var apiKey = "HyFuBrueW"; //YOUR API KEY HERE
+    var url = "https://semantify.it/api/annotation/" + apiKey;
+
+    //preparing dummy annotation
+    var annotation = {};
+    annotation["@context"] = "http://schema.org/";
+    annotation["@type"] = "My Website";
+    annotation["name"] = "My Website's title";
+    annotation["url"] = "http://my-website.org/test";
+
+    //the CID is a unique (inside your account's scope) identifier you can choos for your data
+    var cid = "my-cid-" + "30948ds-fkjslk8_43urlkds-fw4ulskdj_fw93rlkjes";
+
+    //put dummy annotation and cid in new JSON object
+    var annotationObject = {};
+    annotationObject["cid"] = cid;
+    annotationObject["content"] = annotation;
+
+    //push JSON object to JSON array
+    var bulk = [];
+    bulk.push(annotationObject);
+
+    /** dataset should look like
+    * [ {"cid":"<cid>", "content":"<your annotation in JSON-LD>"}, ..., {}]
+    **/
+
+    console.log("URL: %s", url);
+    console.log("Data: %s", JSON.stringify(bulk));
+
+    //send to semantify.it
+    request({
+        url: url,
+        method: "POST",
+        json: bulk
+    }, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(body)
+        } else {
+            console.log("error: " + error)
+            console.log("response.statusCode: " + response.statusCode)
+            console.log("response.statusText: " + response.statusText)
+        }
+    });
+}
